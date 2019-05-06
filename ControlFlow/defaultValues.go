15f7a2672ab8aa6f17afe87e1b935d657f250a65
Suvid <sahay.suvid61@gmail.com>
2019-05-06 20:11:29 +0530

    Formatted output

--- ControlFlow/defaultValues.go
+++ ControlFlow/defaultValues.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 var x int = 25
+
 // x:=25 will not work here as declarative statements are not allowed outside of func body
 
 func main() {
@@ -13,4 +14,11 @@ func main() {
 	fmt.Println(b)
 	fmt.Println(d)
 	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%b\n", x)
+	fmt.Printf("%x\n", x)
+	fmt.Printf("%#x\n", x)
+	s := fmt.Sprintf("%T\n%b\n%x\n", x, x, x)
+	fmt.Println(s)
+
 }
